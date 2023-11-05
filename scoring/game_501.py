dfdcbdd57945eee9a2466658b59d2552c9c9f277
kevinparlak <kevinparlak@outlook.com>
2023-11-05 13:20:21 -0500

    Cleanup logic and make two player

--- scoring/game_501.py
+++ scoring/game_501.py
@@ -1,16 +1,28 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   game_501.py
+@Time    :   2023/11/05
+@Author  :   Kevin Parlak
+@Version :   1.0
+@License :   MIT
+@Desc    :   Class for 501 game
+'''
+import array as arr
+
 class Game501():
     def __init__(self):
-        self.score = 501
-        self.winner = False
+        self.score = arr.array('i', [501, 501])
+        self.winner = arr.array('b', [False, False])
 
-    def set_score(self, score):
-        self.score = score
+    def set_score(self, player, score):
+        self.score[player] = score
 
-    def get_score(self):
-        return self.score
+    def get_score(self, player):
+        return self.score[player]
 
-    def get_winner(self):
-        return self.winner
+    def get_winner(self, player):
+        return self.winner[player]
 
     def calc_hit(self, number, ring):
         if ring == 'B' or ring == 'D':
@@ -26,18 +38,18 @@ class Game501():
         else:
             return 0
 
-    def update(self, number, ring):
+    def update(self, player, number, ring):
         hit = self.calc_hit(number, ring)
-        temp_score = self.score - hit
+        temp_score = self.score[player] - hit
         if temp_score < 0:
-            return self.score
+            return self.score[player]
         elif temp_score == 0:
             if ring == 'A':
-                self.winner = True
+                self.winner[player] = True
                 return temp_score
             else:
-                return self.score
-        else: #temp_score > 0
+                return self.score[player]
+        else: # temp_score > 0
             return temp_score
 
 # EOF
--- scoring/tests/test_game_501.py
+++ scoring/tests/test_game_501.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   test_game_501.py
+@Time    :   2023/11/05
+@Author  :   Kevin Parlak
+@Version :   1.0
+@License :   MIT
+@Desc    :   Class for testing 501 game class
+'''
+
 import unittest
 import sys
 sys.path.append("..")
@@ -8,48 +19,48 @@ class TestGame501(unittest.TestCase):
 
     def test_hit_double(self):
         game = Game501()
-        self.assertEqual(game.update(number=20, ring='A'), 461, 'Double ring hit is wrong')
+        self.assertEqual(game.update(player=0, number=20, ring='A'), 461, 'Double ring hit is wrong')
 
     def test_hit_single_upper(self):
         game = Game501()
-        self.assertEqual(game.update(number=20, ring='B'), 481, 'Single upper ring hit is wrong')
+        self.assertEqual(game.update(player=0, number=20, ring='B'), 481, 'Single upper ring hit is wrong')
 
     def test_hit_triple(self):
         game = Game501()
-        self.assertEqual(game.update(number=20, ring='C'), 441, 'Triple ring hit is wrong')
+        self.assertEqual(game.update(player=0, number=20, ring='C'), 441, 'Triple ring hit is wrong')
 
     def test_hit_single_lower(self):
         game = Game501()
-        self.assertEqual(game.update(number=20, ring='D'), 481, 'Single lower ring hit is wrong')
+        self.assertEqual(game.update(player=0, number=20, ring='D'), 481, 'Single lower ring hit is wrong')
 
     def test_hit_bull(self):
         game = Game501()
-        self.assertEqual(game.update(number=20, ring='X'), 476, 'Bull hit is wrong')
+        self.assertEqual(game.update(player=0, number=20, ring='X'), 476, 'Bull hit is wrong')
 
     def test_hit_bullseye(self):
         game = Game501()
-        self.assertEqual(game.update(number=20, ring='Y'), 451, 'Bullseye hit is wrong')
+        self.assertEqual(game.update(player=0, number=20, ring='Y'), 451, 'Bullseye hit is wrong')
 
     def test_hit_zero(self):
         game = Game501()
-        self.assertEqual(game.update(number=20, ring='Z'), 501, 'Zero hit is wrong')
+        self.assertEqual(game.update(player=0, number=20, ring='Z'), 501, 'Zero hit is wrong')
 
     def test_score_below_zero(self):
         game = Game501()
-        game.set_score(20)
-        self.assertEqual(game.update(number=20, ring='A'), 20, 'Below zero score is wrong')
+        game.set_score(player=0, score=20)
+        self.assertEqual(game.update(player=0, number=20, ring='A'), 20, 'Below zero score is wrong')
 
     def test_score_winner_zero_single(self):
         game = Game501()
-        game.set_score(20)
-        self.assertEqual(game.update(number=20, ring='B'), 20, 'Zero score with single hit is wrong')
-        self.assertEqual(game.get_winner(), False, 'Winner declaration is wrong')
+        game.set_score(player=0, score=20)
+        self.assertEqual(game.update(player=0, number=20, ring='B'), 20, 'Zero score with single hit is wrong')
+        self.assertEqual(game.get_winner(player=0), False, 'Winner declaration is wrong')
 
     def test_score_winner_zero_double(self):
         game = Game501()
-        game.set_score(20)
-        self.assertEqual(game.update(number=10, ring='A'), 0, 'Zero score with double hit is wrong')
-        self.assertEqual(game.get_winner(), True, 'Winner declaration is wrong')
+        game.set_score(player=0, score=20)
+        self.assertEqual(game.update(player=0, number=10, ring='A'), 0, 'Zero score with double hit is wrong')
+        self.assertEqual(game.get_winner(player=0), True, 'Winner declaration is wrong')
 
 if __name__ == '__main__':
     unittest.main()
