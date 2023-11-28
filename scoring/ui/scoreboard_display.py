e3a86847f6e5f93700db5e17a1b397e21b30cb82
kevinparlak <kevinparlak@outlook.com>
2023-11-27 20:03:48 -0500

    Update display

--- scoring/ui/scoreboard_display.py
+++ scoring/ui/scoreboard_display.py
@@ -84,6 +84,16 @@ class Ui_ScoreboardDisplay(object):
         self.quitButton.setText(_translate("ScoreboardDisplay", "Quit"))
         self.player2Button.setText(_translate("ScoreboardDisplay", "Player 2"))
         self.player1Label.setText(_translate("ScoreboardDisplay", "Player:"))
+        self.player1ScoreOutput.setHtml(_translate("ScoreboardDisplay", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:22pt;\"><br /></p></body></html>"))
+        self.player2ScoreOutput.setHtml(_translate("ScoreboardDisplay", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:22pt;\"><br /></p></body></html>"))
         self.player2Label.setText(_translate("ScoreboardDisplay", "Player:"))
 
 
